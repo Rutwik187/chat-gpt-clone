94f63c9db70e31e48e92931ab8d5167d317ed41c
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-02-08 10:34:21 +0530

    fixed typescipt issue while importing roboto_mono font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
 import type { Metadata } from "next";
-import { Roboto_Mono } from "next/font/google";
+import { Roboto_Mono } from 'next/font/google'
+
 import "./globals.css";
 
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
 
 
-export const roboto_mono = Roboto_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export const metadata: Metadata = {
   title: "Bubblspace",
   description: "Prototype of bubblspace",
 };
 
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+ 
+  
   return (
     <html lang="en">
       <body className={roboto_mono.className}>
